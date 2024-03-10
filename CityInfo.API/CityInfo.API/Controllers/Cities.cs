0d6cdc0d65599e9aabc528860ea6dfe04c01978e
iranjith <rgranjith89@gmail.com>
2024-03-10 05:50:19 +0530

    feat: initial changes for cities api

---
+++ CityInfo.API/CityInfo.API/Controllers/Cities.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+
+    public class Cities : ControllerBase
+    {
+        public Cities() { }
+
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object> {
+                    new {id=1, name ="New York"},
+                    new {id=1, name="Tokyo"}
+                });
+        }
+
+
+    }
+}
